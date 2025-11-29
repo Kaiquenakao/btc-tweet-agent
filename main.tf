9f6f6a733e1f0c086b4a499782193a4458dbd4c5
Kaiquenakao <kaiquenakao@gmail.com>
2025-11-28 22:41:11 -0300

    feat: policy para enviar mensagem para o sqs

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 module "ec2" {
   source = "./infra/ec2"
 
-  ami_id    = "ami-0c0039bfde8cbfe27"
-  subnet_id = "subnet-0a1c52410042a044e"
-  vpc_id    = "vpc-0e9225ab2d16f8d9d"
+  ami_id        = var.ami_id
+  subnet_id     = "subnet-0a1c52410042a044e"
+  vpc_id        = "vpc-0e9225ab2d16f8d9d"
+  instance_type = var.instance_type
 
-  instance_type = "t2.micro"
+  sqs_arn = var.sqs_arn
 }
